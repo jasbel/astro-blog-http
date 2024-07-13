190fe7c668355a71c99c93285f5537b582dc94bb
jasbel <asbel.elmec@gmail.com>
2024-07-13 13:41:58 -0400

    add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,19 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap()]
+  // output: "hybrid",
+  // adapter: node({
+  //   mode: "standalone"
+  // })
+  ,
   output: "hybrid",
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: cloudflare()
 });
